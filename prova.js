a8b9bc7f064d58478cd3c768e2b23abe978df4ec
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-04-11 16:57:24 -0300

    Create prova.js

---
+++ prova.js
@@ -0,0 +1,50 @@
+// exercicio 1
+function cumprimentar(nome, idade){
+    console.log(`Olá, ${nome}, sua idade é ${idade}`)
+    }
+    
+    cumprimentar('juliana', 60)
+    cumprimentar('joao', 19)
+    cumprimentar('luis', 26)
+
+//exercicio 2
+letra E 
+
+// exercicio 3 
+// 1. Crie uma função chamada registrarPedido que receba cliente, prato, mesa e idade
+function registrarPedido(cliente, prato,mesa,idade) {
+    return {
+      cliente: ana,
+      prato: feijao,
+      mesa: 12,
+      idade: 23
+    };
+  }
+  // 2. Crie um array vazio chamado pedidos;
+  const pedidos = ('pedidos')
+  // 3. Adicione 3 pedidos ao array usando a função criada
+pedidos.push(registrarPedido1("Ana", "Hambúrguer", 1, 10));
+pedidos.push(registrarPedido2("Bruno", "Lasanha", 2, 17));
+pedidos.push(registrarPedido3("Carlos", "Feijoada", 3, 65));
+
+// 4. Crie uma função chamada classificarIngresso que retorna o tipo com base na idade
+function classificarIngresso(idade) {
+    if (idade <12) {
+      return "IngressoInfantil";
+    } else if (idade>= 12 && idade <=17) {
+      return "Ingressoadolescente";
+    } else if (idade >= 18 && idade <=59) {
+      return "IngressoAdulto";
+    } else {
+      return "Ingressosenhor";
+    }
+  }
+
+//exercicio 4
+letra C
+
+//exercicio 5
+letra C
+
+
+
